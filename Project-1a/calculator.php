95d468a69b62f5bef7913f600507e8475f108c7a
John Kim <thinkndev@gmail.com>
2016-10-04 14:53:58 -0700

    Fixed bugs and cleaned up code
    Resolved dot issue and updated for submission status. Test it out.

--- Project-1a/calculator.php
+++ Project-1a/calculator.php
@@ -18,57 +18,66 @@
 // about eval...
 
 if($_GET["expr"]) { 
+
+	// The equation itself
 	$equ = $_GET["expr"];
 
-	$divide_by_zero = preg_match("/\/[\s]*[0]/", $equ); // v
-	$basic_pattern = preg_match("/^[0-9 \.\+\-\/\*]+$/", $equ); // v
+	// Match the pattern
+	$basic_pattern = preg_match("/^[0-9 \.\+\-\/\*]+$/", $equ);
+
+	// Basic stuff
 	$begin_zero = preg_match("/^[0]+[0-9]+/", $equ); 
-	$zero_num = preg_match("/[ \+\-\/\*]+0[0-9]+/", $equ); // inv
+	$divide_by_zero = preg_match("/\/[\s]*[0]/", $equ);
+	$zero_num = preg_match("/[ \+\-\/\*]+0[0-9]+/", $equ);
 	$inv_ops = preg_match("/[\+\-\/\*][ ]*[\+\/\*]/", $equ);
-	$two_dots = preg_match("/[0-9]+.[ 0-9]+.[ 0-9]+/" , $equ);
 	$negspace = preg_match("/[\+\-\*\/][ ]*\-[ ]+[0-9]/",$equ);
 	$more_than_two_ops = preg_match("/[\+\-\*\/][ ]*[\+\-\*\/][ ]*[\+\-\*\/]/", $equ);
 
-	echo "Division by zero: ".$divide_by_zero."<br \>"; // valid
+	// Dot issues:
+	$start_dot = preg_match("/^\.[ \+\-\*\/]*/", $equ);
+	$end_dot = preg_match("/[ \+\-\*\/]*\.$/", $equ);
+	$left_dot = preg_match("/\.[ \+\-\*\/]+/", $equ);
+	$right_dot = preg_match("/[ \+\-\*\/]+\./", $equ);
+	$two_dots = preg_match("/[0-9]+\.[ 0-9]*\.[ 0-9]*/" , $equ);
+
+/*
+	// Make sure your regex works properly
 	echo "Has basic pattern: ".$basic_pattern."<br \>"; // valid
+	echo "Division by zero: ".$divide_by_zero."<br \>";
 	echo "Begins with zero: ".$begin_zero."<br />";
 	echo "Zero then number: ".$zero_num."<br \>";
-	echo "Invalid operators: ".$inv_ops."<br \>";
-	echo "Invalid floats: ".$two_dots."<br \>";	
+	echo "Invalid operators: ".$inv_ops."<br \>"; 
 	echo "Space in negation: ".$negspace."<br \>";
-	echo "More than two consecutive operators: ".$more_than_two_ops."<br \>";
-
+	echo "2 consec operators: ".$more_than_two_ops."<br \>";
+	echo "Start dot: ".$start_dot."<br \>";
+	echo "End dot: ".$end_dot."<br \>";
+	echo "Left dot: ".$left_dot."<br \>";
+	echo "Right dot: ".$right_dot."<br \>";
+	echo "Two+ dots: ".$two_dots ."<br \>";
+*/
 	echo "<br />";
 
+	echo "<h2>Result</h2>";
 	if($basic_pattern){
 		if($divide_by_zero){
-			echo "Invalid Input";
+			echo "Division by zero error!";
 		}else{
-			if($begin_zero || $zero_num){
-				echo "Invalid Input";
-			}
-			else if ($inv_ops || $two_dots){
-				echo "Invalid Input";
-			}
 			
+			if ($start_dot || $end_dot || $left_dot || $right_dot ||
+				$inv_ops || $more_than_two_ops || $zero_num || 
+				$negspace || $begin_zero || $two_dots){
+				echo "Invalid Expression!";
+			}
 			else{
 				$equ = str_replace("--","- -", $equ);
 				eval("\$ans=$equ ;");
-				echo "hehehe";
+				echo $_GET["expr"]." = ".$ans;
 			}
-	
 		}
 	}
 	else{
-		echo "Invalid Input";
+		echo "Invalid Expression!";
 	}
-	
-	eval("\$ans = $equ ;");
-
-	echo "<h2>Result</h2>";
-	//echo "Expression ".$_GET["expr"]." has been received.<br />"; 
-	echo $_GET["expr"]." = ".$ans;
-
 } 
 ?>
 
