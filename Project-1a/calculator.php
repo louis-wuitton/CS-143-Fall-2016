27347d05d2fe7bf5c716fb3d53850704a5e01906
louis-wuitton <twu1995629@g.ucla.edu>
2016-10-05 17:28:44 -0700

    Fixed some bugs

--- Project-1a/calculator.php
+++ Project-1a/calculator.php
@@ -25,10 +25,14 @@ if($_GET["expr"]) {
 	$begin_zero = preg_match("/^[0]+[0-9]+/", $equ); 
 	$zero_num = preg_match("/[ \+\-\/\*]+0[0-9]+/", $equ); // inv
 	$inv_ops = preg_match("/[\+\-\/\*][ ]*[\+\/\*]/", $equ);
-	$two_dots = preg_match("/[0-9]+.[ 0-9]+.[ 0-9]+/" , $equ);
+	$two_dots = preg_match("/\.[0-9]+\./" , $equ);
 	$negspace = preg_match("/[\+\-\*\/][ ]*\-[ ]+[0-9]/",$equ);
 	$more_than_two_ops = preg_match("/[\+\-\*\/][ ]*[\+\-\*\/][ ]*[\+\-\*\/]/", $equ);
+	$bad_float_1 = preg_match("/[\+\-\*\/ ]\./", $equ);
+	$bad_floar_2 = preg_match("/\.[\+\-\*\/ ]/", $equ);
+	$bad_float_3 = preg_match("/^\./", $equ);	
 
+	/*
 	echo "Division by zero: ".$divide_by_zero."<br \>"; // valid
 	echo "Has basic pattern: ".$basic_pattern."<br \>"; // valid
 	echo "Begins with zero: ".$begin_zero."<br />";
@@ -39,35 +43,38 @@ if($_GET["expr"]) {
 	echo "More than two consecutive operators: ".$more_than_two_ops."<br \>";
 
 	echo "<br />";
+	*/
+
+	
+	
+	
+
 
 	if($basic_pattern){
-		if($divide_by_zero){
-			echo "Invalid Input";
+		if($zero_num || $begin_zero){
+			echo "Invalid Input: A non-zero number cannot start with zero";
 		}else{
-			if($begin_zero || $zero_num){
-				echo "Invalid Input";
+			if($divide_by_zero){
+				echo "Invalid Input: A number that's not zero cannot start with a zero";
 			}
-			else if ($inv_ops || $two_dots){
-				echo "Invalid Input";
+			else if ($inv_ops || $two_dots || $negspace || $more_than_two_ops){
+				echo "Invalid Input: Either invalid arrangement of operators or invalid floating points";
 			}
-			
+			else if($bad_float_1 || $bad_float_2 || $bad_float_3){
+				echo "Invalid Input: All floating point dots have to either precede or be followed by a digit";
+			}		
 			else{
 				$equ = str_replace("--","- -", $equ);
 				eval("\$ans=$equ ;");
-				echo "hehehe";
+				echo "<h2>Result</h2>";
+				echo $_GET["expr"]," = ".$ans;
 			}
 	
 		}
 	}
 	else{
-		echo "Invalid Input";
+		echo "Invalid Input: Input must contain only 0-9, +, -, *, / and . ";
 	}
-	
-	eval("\$ans = $equ ;");
-
-	echo "<h2>Result</h2>";
-	//echo "Expression ".$_GET["expr"]." has been received.<br />"; 
-	echo $_GET["expr"]." = ".$ans;
 
 } 
 ?>
