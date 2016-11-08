60021054f4a764c36c340b983717a0e5864511a8
LouisWu95 <twu1995629@g.ucla.edu>
2016-11-07 22:10:19 -0800

    Added Bruinbase

---
+++ bruinbase/BTreeNode.cc
@@ -0,0 +1,203 @@
+#include "BTreeNode.h"
+
+using namespace std;
+
+/*
+ * Read the content of the node from the page pid in the PageFile pf.
+ * @param pid[IN] the PageId to read
+ * @param pf[IN] PageFile to read from
+ * @return 0 if successful. Return an error code if there is an error.
+ */
+
+// Goal: implement all the functions in this part
+
+RC BTLeafNode::read(PageId pid, const PageFile& pf)
+{
+  //Read into the memory buffer
+  RC rc;
+  if ((rc = pf.read(pid, buffer)) != 0){
+    fprintf(stderr, "Failed to read a node from page file into the buffer");
+  }
+  return rc;
+}
+
+/*
+ * Write the content of the node to the page pid in the PageFile pf.
+ * @param pid[IN] the PageId to write to
+ * @param pf[IN] PageFile to write to
+ * @return 0 if successful. Return an error code if there is an error.
+ */
+RC BTLeafNode::write(PageId pid, PageFile& pf)
+{
+  RC rc;
+  //We could assume that previously you have changed the buffer already
+  if((rc = pf.write(pid, buffer)) != 0){
+    fprintf(stderr, "Failed to write into the page file");
+  }
+  return rc;
+}
+
+/*
+ * Return the number of keys stored in the node.
+ * @return the number of keys in the node
+ */
+
+//We want to fill into the a node as much keys as possible
+
+int BTLeafNode::getKeyCount()
+{
+  //check the main memory and get the number of keys
+  //Meanwhile each leaf node must has a trailing pageid, so the pageid must be reserved for each node
+  //Thus in each node, ideally, we could store as many pairs of recordid and key as possible, and make sure
+  //that eventually we have space to store the trailing page id
+
+  int keySize = sizeof(int) + sizeof(PageId);
+  //Meanwhile we need to consider the trailing 4 bytes
+  int keyCount = 0;
+  
+  for(int i = 0; i <= 1008; i += keySize){
+
+  }
+
+  return 0;
+}
+
+
+/*
+ * Insert a (key, rid) pair to the node.
+ * @param key[IN] the key to insert
+ * @param rid[IN] the RecordId to insert
+ * @return 0 if successful. Return an error code if the node is full.
+ */
+
+RC BTLeafNode::insert(int key, const RecordId& rid)
+{
+  //Keep in mind that we are storing the (key, rid) pair into each node
+  return 0;
+}
+
+
+
+
+
+
+/*
+ * Insert the (key, rid) pair to the node
+ * and split the node half and half with sibling.
+ * The first key of the sibling node is returned in siblingKey.
+ * @param key[IN] the key to insert.
+ * @param rid[IN] the RecordId to insert.
+ * @param sibling[IN] the sibling node to split with. This node MUST be EMPTY when this function is called.
+ * @param siblingKey[OUT] the first key in the sibling node after split.
+ * @return 0 if successful. Return an error code if there is an error.
+ */
+RC BTLeafNode::insertAndSplit(int key, const RecordId& rid,
+                              BTLeafNode& sibling, int& siblingKey)
+{ return 0; }
+
+/**
+ * If searchKey exists in the node, set eid to the index entry
+ * with searchKey and return 0. If not, set eid to the index entry
+ * immediately after the largest index key that is smaller than searchKey,
+ * and return the error code RC_NO_SUCH_RECORD.
+ * Remember that keys inside a B+tree node are always kept sorted.
+ * @param searchKey[IN] the key to search for.
+ * @param eid[OUT] the index entry number with searchKey or immediately
+                   behind the largest key smaller than searchKey.
+ * @return 0 if searchKey is found. Otherwise return an error code.
+ */
+RC BTLeafNode::locate(int searchKey, int& eid)
+{ return 0; }
+
+/*
+ * Read the (key, rid) pair from the eid entry.
+ * @param eid[IN] the entry number to read the (key, rid) pair from
+ * @param key[OUT] the key from the entry
+ * @param rid[OUT] the RecordId from the entry
+ * @return 0 if successful. Return an error code if there is an error.
+ */
+RC BTLeafNode::readEntry(int eid, int& key, RecordId& rid)
+{ return 0; }
+
+/*
+ * Return the pid of the next slibling node.
+ * @return the PageId of the next sibling node
+ */
+PageId BTLeafNode::getNextNodePtr()
+{ return 0; }
+
+/*
+ * Set the pid of the next slibling node.
+ * @param pid[IN] the PageId of the next sibling node
+ * @return 0 if successful. Return an error code if there is an error.
+ */
+RC BTLeafNode::setNextNodePtr(PageId pid)
+{ return 0; }
+
+/*
+ * Read the content of the node from the page pid in the PageFile pf.
+ * @param pid[IN] the PageId to read
+ * @param pf[IN] PageFile to read from
+ * @return 0 if successful. Return an error code if there is an error.
+ */
+RC BTNonLeafNode::read(PageId pid, const PageFile& pf)
+{ return 0; }
+
+/*
+ * Write the content of the node to the page pid in the PageFile pf.
+ * @param pid[IN] the PageId to write to
+ * @param pf[IN] PageFile to write to
+ * @return 0 if successful. Return an error code if there is an error.
+ */
+RC BTNonLeafNode::write(PageId pid, PageFile& pf)
+{ return 0; }
+
+/*
+ * Return the number of keys stored in the node.
+ * @return the number of keys in the node
+ */
+int BTNonLeafNode::getKeyCount()
+{ return 0; }
+
+
+/*
+ * Insert a (key, pid) pair to the node.
+ * @param key[IN] the key to insert
+ * @param pid[IN] the PageId to insert
+ * @return 0 if successful. Return an error code if the node is full.
+ */
+RC BTNonLeafNode::insert(int key, PageId pid)
+{ return 0; }
+
+/*
+ * Insert the (key, pid) pair to the node
+ * and split the node half and half with sibling.
+ * The middle key after the split is returned in midKey.
+ * @param key[IN] the key to insert
+ * @param pid[IN] the PageId to insert
+ * @param sibling[IN] the sibling node to split with. This node MUST be empty when this function is called.
+ * @param midKey[OUT] the key in the middle after the split. This key should be inserted to the parent node.
+ * @return 0 if successful. Return an error code if there is an error.
+ */
+RC BTNonLeafNode::insertAndSplit(int key, PageId pid, BTNonLeafNode& sibling, int& midKey)
+{ return 0; }
+
+/*
+ * Given the searchKey, find the child-node pointer to follow and
+ * output it in pid.
+ * @param searchKey[IN] the searchKey that is being looked up.
+ * @param pid[OUT] the pointer to the child node to follow.
+ * @return 0 if successful. Return an error code if there is an error.
+ */
+RC BTNonLeafNode::locateChildPtr(int searchKey, PageId& pid)
+{ return 0; }
+
+/*
+ * Initialize the root node with (pid1, key, pid2).
+ * @param pid1[IN] the first PageId to insert
+ * @param key[IN] the key that should be inserted between the two PageIds
+ * @param pid2[IN] the PageId to insert behind the key
+ * @return 0 if successful. Return an error code if there is an error.
+ */
+RC BTNonLeafNode::initializeRoot(PageId pid1, int key, PageId pid2)
+{ return 0; }
